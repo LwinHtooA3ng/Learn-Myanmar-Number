6d1d1ea08a2e4780d1643ff93449b05cb20ddb93
Lwin Htoo Aung <lwinhtooaung267@gmail.com>
2022-07-15 11:43:26 +0630

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Myanmar Number',
       home: Scaffold(
         appBar: AppBar(
